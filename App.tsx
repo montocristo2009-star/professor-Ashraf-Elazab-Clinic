6cba68c741fae5a35e4f5f4bc6df1dd36f9b9483
montocristo2009-star <montocristo2009@gmail.com>
2026-02-01 00:33:24 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,44 +1,15 @@
-import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar.tsx';
-import Footer from './components/Footer.tsx';
-import SocialSidebar from './components/SocialSidebar.tsx';
-import Home from './pages/Home.tsx';
-import About from './pages/About.tsx';
-import Booking from './pages/Booking.tsx';
-import Blog from './pages/Blog.tsx';
-import Testimonials from './pages/Testimonials.tsx';
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [pathname]);
-  return null;
-};
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import AiAssistant from './AiAssistant';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen bg-white font-cairo" dir="rtl">
-        <Navbar />
-        <SocialSidebar />
-        
-        <main className="flex-grow pt-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Home />} />
-            <Route path="/blog" element={<Blog />} />     
-            <Route path="/testimonials" element={<Testimonials />} />
-            <Route path="/booking" element={<Booking />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </main>
-        
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai-assistant" element={<AiAssistant />} />
+      </Routes>
     </Router>
   );
 };
